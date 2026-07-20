82b9da235913bbff4150fda1f8bc0b5897dac09f
bgm3167 <144885221+bgm3167@users.noreply.github.com>
2026-07-20 12:28:54 +0900

    [프로그래머스] L0 - 편지 (#15)

---
+++ programmers/L0_기초입문/입문/015_편지.py
@@ -0,0 +1,9 @@
+# 편지
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120898
+# 알고리즘: 기초
+# 작성자: 백관민
+# 작성일: 2026. 07. 20. 12:28:53
+
+def solution(message):
+    return(2*len(message))
