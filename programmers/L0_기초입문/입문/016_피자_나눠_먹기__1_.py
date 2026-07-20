67dc66d1e55990c48403e9ff23c07c1953269c9a
bgm3167 <144885221+bgm3167@users.noreply.github.com>
2026-07-20 09:46:48 +0900

    [프로그래머스] L0 - 피자 나눠 먹기 (1) (#16)

---
+++ programmers/L0_기초입문/입문/016_피자_나눠_먹기__1_.py
@@ -0,0 +1,11 @@
+# 피자 나눠 먹기 (1)
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120814
+# 알고리즘: 기초
+# 작성자: 백관민
+# 작성일: 2026. 07. 20. 09:46:44
+
+import math as ma
+
+def solution(n):
+    return ma.ceil(n/7)
