c4300c4b0e714a457793ba53b5a6a258191fd5dd
bgm3167 <144885221+bgm3167@users.noreply.github.com>
2026-07-21 10:29:26 +0900

    [프로그래머스] L0 - 합성수 찾기 (#59)

--- programmers/L0_기초입문/입문/059_합성수_찾기.py
+++ programmers/L0_기초입문/입문/059_합성수_찾기.py
@@ -3,18 +3,18 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120846
 # 알고리즘: 기초
 # 작성자: 백관민
-# 작성일: 2026. 07. 21. 10:28:26
+# 작성일: 2026. 07. 21. 10:29:25
 
 def solution(n):
     answer = 0
 
     for i in range(1, n + 1):
         count = 0
-        for j in range(1, i + 1):
+        for j in range(1, int(i**0.5) + 1):
             if i % j == 0:
                 count += 1
 
-        if count > 2:
+        if count > 1:
             answer += 1
 
     return answer
