aa3d63644d9a3b18066c6f75ff1d5d8db28fa0b6
bgm3167 <144885221+bgm3167@users.noreply.github.com>
2026-07-21 09:58:40 +0900

    [프로그래머스] L0 - 대문자와 소문자 (#54)

---
+++ programmers/L0_기초입문/입문/054_대문자와_소문자.py
@@ -0,0 +1,9 @@
+# 대문자와 소문자
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120893
+# 알고리즘: 기초
+# 작성자: 백관민
+# 작성일: 2026. 07. 21. 09:58:39
+
+def solution(my_string):
+    return my_string.swapcase()
