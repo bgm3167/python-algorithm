f1d15acaae1def82847d5438c0afa02fe8c1d2c3
bgm3167 <144885221+bgm3167@users.noreply.github.com>
2026-07-22 10:34:17 +0900

    [프로그래머스] L0 - 종이 자르기 (#81)

---
+++ programmers/L0_기초입문/입문/081_종이_자르기.py
@@ -0,0 +1,9 @@
+# 종이 자르기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120922
+# 알고리즘: 기초
+# 작성자: 백관민
+# 작성일: 2026. 07. 22. 10:34:15
+
+def solution(M, N):
+    return M * N -1
