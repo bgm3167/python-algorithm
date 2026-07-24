ba89f739caa801f5b08ff743dfb3310b5ada8a7b
bgm3167 <144885221+bgm3167@users.noreply.github.com>
2026-07-24 10:41:35 +0900

    [프로그래머스] L0 - n 번째 원소부터 (#115)

---
+++ programmers/L0_기초입문/기초트레이닝/115_n_번째_원소부터.py
@@ -0,0 +1,9 @@
+# n 번째 원소부터
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181892
+# 알고리즘: 리스트(배열)
+# 작성자: 백관민
+# 작성일: 2026. 07. 24. 10:41:34
+
+def solution(num_list, n):
+    return num_list[n-1:]
