0e2ad69ca65f02055989998578ce9763d7a0a7f1
bgm3167 <144885221+bgm3167@users.noreply.github.com>
2026-07-24 10:37:13 +0900

    [프로그래머스] L0 - n의 배수 (#112)

---
+++ programmers/L0_기초입문/기초트레이닝/112_n의_배수.py
@@ -0,0 +1,9 @@
+# n의 배수
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181937
+# 알고리즘: 연산
+# 작성자: 백관민
+# 작성일: 2026. 07. 24. 10:37:13
+
+def solution(num, n):
+    return 1 if num % n == 0 else 0
