c9b58d9425274226b83f823c0248289fd6e900c2
bgm3167 <144885221+bgm3167@users.noreply.github.com>
2026-07-24 15:59:58 +0900

    [프로그래머스] L0 - 뒤에서 5등까지 (#141)

---
+++ programmers/L0_기초입문/기초트레이닝/141_뒤에서_5등까지.py
@@ -0,0 +1,9 @@
+# 뒤에서 5등까지
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181853
+# 알고리즘: 함수(메서드)
+# 작성자: 백관민
+# 작성일: 2026. 07. 24. 15:59:57
+
+def solution(num_list):
+    return sorted(num_list)[:5]
