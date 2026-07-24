aeb1fb78b5de368b4d3fc67d538fba7a27f7043d
bgm3167 <144885221+bgm3167@users.noreply.github.com>
2026-07-24 12:36:45 +0900

    [프로그래머스] L0 - 부분 문자열인지 확인하기 (#136)

---
+++ programmers/L0_기초입문/기초트레이닝/136_부분_문자열인지_확인하기.py
@@ -0,0 +1,9 @@
+# 부분 문자열인지 확인하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181843
+# 알고리즘: 조건문 활용
+# 작성자: 백관민
+# 작성일: 2026. 07. 24. 12:36:44
+
+def solution(my_string, target):
+    return int(target in my_string)
