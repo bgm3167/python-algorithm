fac11c1fec19bf032bba9e777924e50958a62f14
bgm3167 <144885221+bgm3167@users.noreply.github.com>
2026-07-24 13:58:56 +0900

    [프로그래머스] L0 - 문자열을 정수로 변환하기 (#140)

---
+++ programmers/L0_기초입문/기초트레이닝/140_문자열을_정수로_변환하기.py
@@ -0,0 +1,9 @@
+# 문자열을 정수로 변환하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181848
+# 알고리즘: 함수(메서드)
+# 작성자: 백관민
+# 작성일: 2026. 07. 24. 13:58:56
+
+def solution(n_str):
+    return int(n_str)
