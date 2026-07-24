b2ea406b7053dc965c85bb792a50d2e623d186f3
bgm3167 <144885221+bgm3167@users.noreply.github.com>
2026-07-24 11:15:42 +0900

    [프로그래머스] L0 - 덧셈식 출력하기 (#125)

---
+++ programmers/L0_기초입문/기초트레이닝/125_덧셈식_출력하기.py
@@ -0,0 +1,9 @@
+# 덧셈식 출력하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181947
+# 알고리즘: 출력
+# 작성자: 백관민
+# 작성일: 2026. 07. 24. 11:15:41
+
+a, b = map(int, input().strip().split(' '))
+print(f"{a} + {b} = {a+b}")
