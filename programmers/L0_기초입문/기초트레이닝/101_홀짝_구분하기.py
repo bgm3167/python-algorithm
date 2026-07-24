2e6c6b9720699ddec4d56033681b1b15b54784ac
bgm3167 <144885221+bgm3167@users.noreply.github.com>
2026-07-24 10:01:41 +0900

    [프로그래머스] L0 - 홀짝 구분하기 (#101)

---
+++ programmers/L0_기초입문/기초트레이닝/101_홀짝_구분하기.py
@@ -0,0 +1,14 @@
+# 홀짝 구분하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181944
+# 알고리즘: 출력
+# 작성자: 백관민
+# 작성일: 2026. 07. 24. 10:01:41
+
+a = int(input())
+
+if a % 2 == 0:
+    print(f"{a} is even")
+
+else:
+    print(f"{a} is odd")
