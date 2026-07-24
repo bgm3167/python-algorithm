24863c097d67a692a6786025591d7afb4a3f74b4
bgm3167 <144885221+bgm3167@users.noreply.github.com>
2026-07-24 10:17:22 +0900

    [프로그래머스] L0 - 대문자로 바꾸기 (#104)

---
+++ programmers/L0_기초입문/기초트레이닝/104_대문자로_바꾸기.py
@@ -0,0 +1,9 @@
+# 대문자로 바꾸기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181877
+# 알고리즘: 문자열
+# 작성자: 백관민
+# 작성일: 2026. 07. 24. 10:17:21
+
+def solution(myString):
+    return myString.upper()
