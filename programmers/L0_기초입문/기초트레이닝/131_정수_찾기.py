c56a5f2eed47527e490b5069dc00252baeb9dae5
bgm3167 <144885221+bgm3167@users.noreply.github.com>
2026-07-24 11:38:48 +0900

    [프로그래머스] L0 - 정수 찾기 (#131)

---
+++ programmers/L0_기초입문/기초트레이닝/131_정수_찾기.py
@@ -0,0 +1,9 @@
+# 정수 찾기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181840
+# 알고리즘: 조건문 활용
+# 작성자: 백관민
+# 작성일: 2026. 07. 24. 11:38:47
+
+def solution(num_list, n):
+    return 1 if n in num_list else 0
