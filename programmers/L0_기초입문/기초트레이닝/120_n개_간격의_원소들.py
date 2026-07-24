d80281ea30e49d2e9a53e993fabe4de70f6af746
bgm3167 <144885221+bgm3167@users.noreply.github.com>
2026-07-24 10:56:30 +0900

    [프로그래머스] L0 - n개 간격의 원소들 (#120)

---
+++ programmers/L0_기초입문/기초트레이닝/120_n개_간격의_원소들.py
@@ -0,0 +1,9 @@
+# n개 간격의 원소들
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181888
+# 알고리즘: 리스트(배열)
+# 작성자: 백관민
+# 작성일: 2026. 07. 24. 10:56:29
+
+def solution(num_list, n):
+    return num_list[::n]
