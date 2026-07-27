15ba4671125ceef7dd4baa804ec69ef9313db67b
bgm3167 <144885221+bgm3167@users.noreply.github.com>
2026-07-27 09:34:27 +0900

    [프로그래머스] L0 - 다항식 더하기 (#91)

--- programmers/L0_기초입문/입문/091_다항식_더하기.py
+++ programmers/L0_기초입문/입문/091_다항식_더하기.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120863
 # 알고리즘: 기초
 # 작성자: 백관민
-# 작성일: 2026. 07. 27. 09:12:45
+# 작성일: 2026. 07. 27. 09:34:26
 
 def solution(polynomial):
     x_sum = 0       # x항의 계수 합
