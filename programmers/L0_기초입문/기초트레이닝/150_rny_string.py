1e33bd44690d0a4cf19acaad94910800a1fbb9d2
bgm3167 <144885221+bgm3167@users.noreply.github.com>
2026-07-28 13:56:50 +0900

    [프로그래머스] L0 - rny_string (#150)

---
+++ programmers/L0_기초입문/기초트레이닝/150_rny_string.py
@@ -0,0 +1,9 @@
+# rny_string
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181863
+# 알고리즘: 문자열
+# 작성자: 백관민
+# 작성일: 2026. 07. 28. 13:56:50
+
+def solution(rny_string):
+    return rny_string.replace('m','rn')
