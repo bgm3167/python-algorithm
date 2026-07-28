8821eb9ed01daf6a160cbed4fd5e7441c5b1f32c
bgm3167 <144885221+bgm3167@users.noreply.github.com>
2026-07-28 16:08:50 +0900

    [프로그래머스] L0 - A 강조하기 (#164)

---
+++ programmers/L0_기초입문/기초트레이닝/164_A_강조하기.py
@@ -0,0 +1,9 @@
+# A 강조하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181874
+# 알고리즘: 문자열
+# 작성자: 백관민
+# 작성일: 2026. 07. 28. 16:08:50
+
+def solution(myString):
+    return myString.lower().replace('a', 'A')
