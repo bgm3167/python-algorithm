bca3f5d72de11b5babf27d76c079fdbd6adfc61b
bgm3167 <144885221+bgm3167@users.noreply.github.com>
2026-07-29 10:16:31 +0900

    [프로그래머스] 접두사인지 확인하기 풀이 제출
    - 문제 ID: 181906
    - 난이도: unknown
    - 제출일: 2026. 07. 29. 10:16:30
    - 작성자: 백관민

---
+++ programmers/other/181906_접두사인지_확인하기.py
@@ -0,0 +1,8 @@
+# 접두사인지 확인하기
+# 프로그래머스 (unknown)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181906
+# 작성자: 백관민
+# 작성일: 2026. 07. 29. 10:16:30
+
+def solution(my_string, is_prefix):
+    return int(my_string.startswith(is_prefix))
