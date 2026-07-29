9ac3873804da1c0abc99bd4240536803a888e7d2
bgm3167 <144885221+bgm3167@users.noreply.github.com>
2026-07-29 09:18:30 +0900

    [프로그래머스] L0 - 소문자로 바꾸기 (#154)

---
+++ programmers/L0_기초입문/기초트레이닝/154_소문자로_바꾸기.py
@@ -0,0 +1,9 @@
+# 소문자로 바꾸기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181876
+# 알고리즘: 문자열
+# 작성자: 백관민
+# 작성일: 2026. 07. 29. 09:18:30
+
+def solution(myString):
+    return myString.lower()
