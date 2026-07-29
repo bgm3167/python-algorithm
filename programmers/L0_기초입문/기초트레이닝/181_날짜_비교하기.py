156bdfe7a4c075929a89fc66362e359d2b53a2f2
bgm3167 <144885221+bgm3167@users.noreply.github.com>
2026-07-29 11:16:17 +0900

    [프로그래머스] L0 - 날짜 비교하기 (#181)

---
+++ programmers/L0_기초입문/기초트레이닝/181_날짜_비교하기.py
@@ -0,0 +1,10 @@
+# 날짜 비교하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181838
+# 알고리즘: 조건문 활용
+# 작성자: 백관민
+# 작성일: 2026. 07. 29. 11:16:16
+
+def solution(date1, date2):
+    return 1 if date1 < date2 else 0
+            
