9106097fdd2bad0c9ad011039b510b114d4eded3
bgm3167 <144885221+bgm3167@users.noreply.github.com>
2026-07-30 09:37:33 +0900

    [프로그래머스] L0 - 문자열 뒤집기 (#187)

---
+++ programmers/L0_기초입문/기초트레이닝/187_문자열_뒤집기.py
@@ -0,0 +1,14 @@
+# 문자열 뒤집기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181905
+# 알고리즘: 문자열
+# 작성자: 백관민
+# 작성일: 2026. 07. 30. 09:37:33
+
+def solution(my_string, s, e):
+    return my_string[:s] + my_string[s:e+1][::-1] + my_string[e+1:]
+
+
+    
+    
+    
