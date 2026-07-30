2555042f69ec428739aba62bb5b8dedf79f69164
bgm3167 <144885221+bgm3167@users.noreply.github.com>
2026-07-30 09:05:16 +0900

    [프로그래머스] L0 - 문자열 묶기 (#192)

---
+++ programmers/L0_기초입문/기초트레이닝/192_문자열_묶기.py
@@ -0,0 +1,13 @@
+# 문자열 묶기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181855
+# 알고리즘: 함수(메서드)
+# 작성자: 백관민
+# 작성일: 2026. 07. 30. 09:05:15
+
+def solution(strArr):
+    a = []
+    for i in strArr:
+        a.append(str(len(i)))
+    b = (max(a, key=a.count))
+    return a.count(b)
