dd13eda2273907eec394e432c58f90c6fc2a291f
bgm3167 <144885221+bgm3167@users.noreply.github.com>
2026-08-06 10:14:46 +0900

    [프로그래머스] L0 - 문자열 출력하기 (#221)

---
+++ programmers/L0_기초입문/기초트레이닝/221_문자열_출력하기.py
@@ -0,0 +1,10 @@
+# 문자열 출력하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181952
+# 알고리즘: 출력
+# 작성자: 백관민
+# 작성일: 2026. 08. 06. 10:14:45
+
+str = input()
+
+print(str)
