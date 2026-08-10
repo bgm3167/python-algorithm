950811cb8e35e159d889252f93d3f7e782640bc4
bgm3167 <144885221+bgm3167@users.noreply.github.com>
2026-08-10 09:34:23 +0900

    [프로그래머스] L2 - 콜라츠 추측 (#236)

---
+++ programmers/L2_기초/236_콜라츠_추측.py
@@ -0,0 +1,19 @@
+# 콜라츠 추측
+# 프로그래머스 L2 (기초)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12943
+# 알고리즘: 시뮬레이션, 반복문
+# 작성자: 백관민
+# 작성일: 2026. 08. 10. 09:34:22
+
+def solution(num):
+    n = 0
+    while num > 1:
+        if n == 500:
+            return -1
+            break
+        if num % 2 == 0:
+            num = num / 2
+        else:
+            num = 3 * num +1
+        n += 1
+    return n
