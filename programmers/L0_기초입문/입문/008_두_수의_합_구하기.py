9f2d69c06530cb185378ecb7aa98dec51aa38788
bgm3167 <144885221+bgm3167@users.noreply.github.com>
2026-08-10 09:06:53 +0900

    [프로그래머스] L0 - 두 수의 합 구하기 (#8)

--- programmers/L0_기초입문/입문/008_두_수의_합_구하기.py
+++ programmers/L0_기초입문/입문/008_두_수의_합_구하기.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120802
 # 알고리즘: 기초
 # 작성자: 백관민
-# 작성일: 2026. 07. 16. 17:46:30
+# 작성일: 2026. 08. 10. 09:06:53
 
 def solution(num1, num2):
     return num1 + num2
